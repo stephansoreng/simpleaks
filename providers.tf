3ea2024e4bae0489f98d77d10f85cc5dc64b9e39
Stephan  Kristiansen <soreng@sskconsulting3.onmicrosoft.com>
2022-11-16 12:38:02 +0100

    commit

--- providers.tf
+++ providers.tf
@@ -10,25 +10,9 @@ terraform {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
-    kubernetes = {
-      source  = "kubernetes"
-      version = "=2.15.0"
-    }
   }
 }
 
 provider "azurerm" {
   features {}
-}
-
-data "azurerm_kubernetes_cluster" "k8s" {
-  name                = "${azurerm_resource_group.rg.name}-cluster"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-provider "kubernetes" {
-  host = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config.0.cluster_ca_certificate)
 }
